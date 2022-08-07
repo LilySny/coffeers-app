b821aa6ba5de6897965b7c20f6e547dd4b92390f
LilySny <liviacastilholi@gmail.com>
2022-08-07 17:39:59 -0300

    add access location from android native

--- android/app/src/main/kotlin/com/example/coffers_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/coffers_app/MainActivity.kt
@@ -1,6 +1,64 @@
 package com.example.coffers_app
 
+import android.app.AlertDialog
+import android.content.Context
+import android.content.Intent
+import android.location.LocationManager
+import android.net.Uri
+import android.provider.Settings
+import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
+
 
 class MainActivity: FlutterActivity() {
+
+
+    private val channelName = "com.example.coffers_app/coffees-location"
+
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, channelName)
+        .setMethodCallHandler {
+            call, result ->
+            if (call.method == "coffees-location"){
+                statusCheck()
+                result.success("Teste5")
+
+            } else result.notImplemented()
+
+        }
+    }
+
+    private fun openMap(){
+        val gmmIntentUri =
+            Uri.parse("geo:0,0?z=10&q=coffees")
+        val mapIntent = Intent(Intent.ACTION_VIEW, gmmIntentUri)
+        mapIntent.setPackage("com.google.android.apps.maps")
+        startActivity(mapIntent)
+    }
+
+
+    private fun statusCheck() {
+        val manager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        if (!manager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            buildAlertMessageNoGps()
+        } else openMap()
+    }
+
+    private fun buildAlertMessageNoGps() {
+        val builder = AlertDialog.Builder(this)
+        builder.setMessage("Ative sua localização para localizarmos as melhores cafeterias perto de você!")
+            .setCancelable(false)
+            .setPositiveButton(
+                "Sim"
+            ) { dialog, id -> startActivity(Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)) }
+            .setNegativeButton(
+                "Não"
+            ) { dialog, id -> dialog.cancel() }
+        val alert = builder.create()
+        alert.show()
+    }
+
 }
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,10 +1,45 @@
-import 'package:flutter/material.dart';
+import 'dart:developer';
 
-class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class HomePage extends StatefulWidget {
+  HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final channelName = 'com.example.coffers_app/coffees-location';
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: ElevatedButton(
+                onPressed: () {
+                  accessLocation();
+                },
+                child: Text("Abrir Mapa")),
+          )
+        ],
+      ),
+    );
+  }
+
+  Future accessLocation() async {
+    try {
+      final methodChannel = MethodChannel(channelName);
+      final String result =
+          await methodChannel.invokeMethod("coffees-location");
+      log(result);
+    } on PlatformException catch (err) {
+      log("Error invoking method $err");
+    }
   }
 }
